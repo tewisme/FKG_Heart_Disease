4ce3dd1134f0927e08c025d3731f412074e9f9e4
tew <demohandsome@gmail.com>
2025-09-07 16:46:05 +0700

    third commit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
+import proc_data
 from skfuzzy import control as ctrl
 
 df = pd.read_csv('../../data/Heart.csv')
+
+proc_data.visualization(df)
